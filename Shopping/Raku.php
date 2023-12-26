59a9e9eba6755a8e98740cca2a8c72f4dc34cbfe
Julius Caezar C. Catubig <130309182+Y05-C4TUB16@users.noreply.github.com>
2023-12-26 09:38:56 +0800

    Added Css

--- Shopping/Raku.php
+++ Shopping/Raku.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>RAKU</title>
+    <link rel="stylesheet" href="Raku.css">
 </head>
 <body>
-    <h1>RAKU CLOTHING</h1>
+<nav class="navhead">
+  <ul class="main-nav">
+    <li><a href="">Men</a></li>
+    <li><a href="">Women</a></li>
+    <li><a href="">Explore</a></li>
+  </ul>
+</nav>
+
 </body>
 </html>
