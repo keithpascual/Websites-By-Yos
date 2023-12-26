9608d6a72c5e2e2b38117eb1106fa94d1a01ff7e
Julius Caezar C. Catubig <130309182+Y05-C4TUB16@users.noreply.github.com>
2023-12-27 01:51:13 +0800

    Navigation
    Finish navigation bar CSS

--- Shopping/Raku.php
+++ Shopping/Raku.php
@@ -18,7 +18,20 @@
                 <li class="item item-1"><a href="#">Explore</a></li>
             </ul>
         <h1><a href="#" class="logo ">R A K U</a></h1>
+        
+        <ul class="icon-navigations">
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
 
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1"/>
+            </svg>
+
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 18">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm-2 3h4a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z"/>
+            </svg>
+        </ul>
     </header>
 </body>
 </html>
