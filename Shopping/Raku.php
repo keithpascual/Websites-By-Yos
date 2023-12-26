7ec7c2bc39cbc74fdbc4b5fa9b363af863b5f572
Julius Caezar C. Catubig <130309182+Y05-C4TUB16@users.noreply.github.com>
2023-12-26 10:00:40 +0800

    Navigation

--- Shopping/Raku.php
+++ Shopping/Raku.php
@@ -5,14 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RAKU</title>
     <link rel="stylesheet" href="Raku.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300&display=swap" rel="stylesheet">
 </head>
 <body>
-<nav class="navhead">
-  <ul class="main-nav">
-    <li><a href="">Men</a></li>
-    <li><a href="">Women</a></li>
-    <li><a href="">Explore</a></li>
-  </ul>
+    <nav class="navhead">
+        <ul class="main-nav">
+            <li class="logo"><a href="">楽</a></li>
+            <li><a href="">Men</a></li>
+            <li><a href="">Women</a></li>
+            <li><a href="">Explore</a></li>
+        </ul>
 </nav>
 
 </body>
