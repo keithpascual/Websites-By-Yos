2653690ce8bf28926ca847c60e9a13e7183d5dd9
Julius Caezar C. Catubig <130309182+Y05-C4TUB16@users.noreply.github.com>
2024-01-13 00:10:42 +0800

    Raku Homepage
    Changed the background picture and fix few adjustment

--- Shopping/Raku.php
+++ Shopping/Raku.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
@@ -9,35 +10,36 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300&display=swap" rel="stylesheet">
-</header>
+    </header>
+
 <body>
     <div class="nav-container">
-        
-            <ul class="navigations">
-                <h1 class="symbol"><a href="Raku.php">楽</a></h1>
-                <li class="item item-1"><a href="mens.php">Men</a></li>
-                <li class="item item-1"><a href="#">Contacts</a></li>
-                <li class="item item-1"><a href="#">About us</a></li>
-            </ul>
+
+        <ul class="navigations">
+            <h1 class="symbol"><a href="Raku.php">楽</a></h1>
+            <li class="item item-1"><a href="mens.php">Men</a></li>
+            <li class="item item-1"><a href="#">Contacts</a></li>
+            <li class="item item-1"><a href="#">About us</a></li>
+        </ul>
 
         <h1><a href="#" class="logo ">R A K U</a></h1>
-        
+
         <ul class="icon-navigations">
-            
+
             <li class="icon-1"><a href="#"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                </svg>
-            </a></li>
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                </a></li>
 
             <li class="icon-2"><a href="#"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1"/>
-                </svg>
-            </a></li>
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1" />
+                    </svg>
+                </a></li>
 
             <li class="icon-3"><a href="#"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 18">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm-2 3h4a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z"/>
-                </svg>
-            </a></li>
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm-2 3h4a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z" />
+                    </svg>
+                </a></li>
         </ul>
 
     </div>
@@ -45,17 +47,20 @@
     <!-- Content -->
 
     <div class="text-container">
-    <div class="text-1"><h1><strong>Summer is almost here!</strong></h1></div>
-    <div class="paragraph">
-        <p>Welcome to RAKU, where fashion is always in season! 
-        Browse our collection and get ready to elevate your summer style!</p>
+        <div class="text-box">
+            <div class="text-1">
+                <h1><strong>Summer is almost here!</strong></h1>
+            </div>
+            <div class="paragraph">
+                <p>Welcome to RAKU, where fashion is always in season!
+                    Browse our collection and get ready to elevate your summer style!</p>
+            </div>
+            <a href="mens.php"><button>EXPLORE NEW SUMMER STYLE</button></a>
+        </div>
     </div>
-    <a href="mens.php"><button>EXPLORE NEW SUMMER STYLE</button></a>
 
-    <img class="model" src="img/Rakumodel.jpg" alt="">
-    </div>
+    <img class="model" src="img/Rakumodel.png" alt="Raku Summer Fashion Model">
 
-    
 
     <!-- Footer -->
 
@@ -94,4 +99,5 @@
         <p>© 2023 ALL RIGHTS RESERVED | <a href="#">PRIVACY</a> | <a href="#">TERMS OF USE</a></p>
     </div>
 </body>
+
 </html>
