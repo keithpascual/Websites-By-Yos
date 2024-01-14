890a0a5381fb7b0689e5133863e8ea0264e9e494
Julius Caezar C. Catubig <130309182+Y05-C4TUB16@users.noreply.github.com>
2024-01-14 19:08:26 +0800

    Raku.php
    Removed the icon

--- Shopping/Raku.php
+++ Shopping/Raku.php
@@ -35,11 +35,6 @@
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1" />
                     </svg>
                 </a></li>
-
-            <li class="icon-3"><a href="#"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 18">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm-2 3h4a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z" />
-                    </svg>
-                </a></li>
         </ul>
 
     </div>
